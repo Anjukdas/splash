e386a23ba48b1919919b1295553fce1f415ffc42
Anjukdas <anjukollara@gmail.com>
2021-07-16 11:46:05 +0530

    splashpage

---
+++ app/src/main/java/com/anju/splashone/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.anju.splashone
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+
+    private val splashScreentimeout: Long =2500
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        Handler().postDelayed({
+            startActivity(Intent(this,Login::class.java))
+            finish()
+        },splashScreentimeout)
+
+    }
+}
